44890f72c759b825049effe87bb10c4f6efc9078
QrystQ <K_rystian.k@wp.pl>
2025-02-25 22:56:18 +0100

    Clean up code

--- Assets/!/Code/Scripts/Player/PlayerMovement.cs
+++ Assets/!/Code/Scripts/Player/PlayerMovement.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float sprintMultiplier = 1.5f;
-    public LayerMask groundLayer;
-    public Transform groundCheck;
-
+ 
     private Rigidbody2D rb;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
     void Update()
