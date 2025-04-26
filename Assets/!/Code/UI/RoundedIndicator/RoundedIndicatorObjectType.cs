6d3bf24a835f973ee37bed811fcc3dec102640e3
Kuryło.ao <kurylo.ao@gmail.com>
2025-04-26 22:48:44 +0200

    feat: added rounded indicator without colors (#8)

---
+++ Assets/!/Code/UI/RoundedIndicator/RoundedIndicatorObjectType.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public enum RoundedIndicatorObject
+{
+    ENEMY,
+    MEMORY,
+    ARTEFACT,
+    Enemies,
+}
+
+public class RoundedIndicatorObjectType : MonoBehaviour
+{
+    public RoundedIndicatorObject type;
+}
